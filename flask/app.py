c5caf8a52b22ae4ac71f00744b96563a479bd7aa
nikita-Zerobit <n.baranovskiy00@gmail.com>
2022-12-14 23:55:27 +0300

    add new pages and style them

--- flask/app.py
+++ flask/app.py
@@ -8,9 +8,40 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/about-us')
-def about():
-    return render_template('account.html')
+@app.route('/account')
+def account():
+    menu_point = {'type': 'account'}
+    return render_template('account.html', context=menu_point)
+
+
+@app.route('/account-info')
+def account_info():
+    menu_point = {'type': 'account'}
+    return render_template('account.html', context=menu_point)
+
+
+@app.route('/payments')
+def payments():
+    menu_point = {'type': 'payments'}
+    return render_template('account.html', context=menu_point)
+
+
+@app.route('/upload-file')
+def upload_file():
+    menu_point = {'type': 'upload'}
+    return render_template('account.html', context=menu_point)
+
+
+@app.route('/analytics')
+def analytics():
+    menu_point = {'type': 'analytics'}
+    return render_template('account.html', context=menu_point)
+
+
+@app.route('/faq')
+def faq():
+    menu_point = {'type': 'faq.html'}
+    return render_template('account.html', context=menu_point)
 
 
 if __name__ == '__main__':
